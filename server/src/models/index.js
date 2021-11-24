223f2c761156abb41d9c93f727943641534b9adf
Quentin Davillé <quentin.daville@epitech.eu>
2021-11-24 16:38:31 +0100

    [Merge]

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,32 +1,32 @@
-'use strict';
+// 'use strict';
 
-// Constant
-const {POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT} = process.env
-const {Sequelize, DataTypes} = require("sequelize");
+// // Constant
+// const {POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT} = process.env
+// const {Sequelize, DataTypes} = require("sequelize");
 
-console.log(POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT)
-const sq_user = new Sequelize("user", POSTGRES_USERNAME, POSTGRES_PASSWORD, {
-  host: POSTGRES_HOST,
-  // port: POSTGRES_PORT,
-  dialect: "postgres",
-})
+// console.log(POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT)
+// const sq_user = new Sequelize("user", POSTGRES_USERNAME, POSTGRES_PASSWORD, {
+//   host: POSTGRES_HOST,
+//   // port: POSTGRES_PORT,
+//   dialect: "postgres",
+// })
 
-sq_user.authenticate().then(() => {
-  console.log("Connection to user database has been established sucessfully.");
-}).catch(err => {
-  console.log("Error: ", err);
-});
+// sq_user.authenticate().then(() => {
+//   console.log("Connection to user database has been established sucessfully.");
+// }).catch(err => {
+//   console.log("Error: ", err);
+// });
 
-const User = sq_user.define("user", {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
-});
-User.sync();
+// const User = sq_user.define("user", {
+//   username: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   }
+// });
+// User.sync();
 
-module.exports = sq_user;
+// module.exports = sq_user;
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-
-'use strict';
-
-=======
-const express = require("express");
->>>>>>> 70554335aa31c6a222753874b116e46284f963ee
-
 // Constant
 const PORT = 8080
 const HOST = "0.0.0.0";
 const express = require("express");
 const serverRouter = require("./routes/routes.js");
-const databaseConnector = require("./models/index.js")
 
 // App
 const app = express();
