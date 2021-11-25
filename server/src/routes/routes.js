2882b1ced1b0ea322649f6f7d8c0ad50472aa7af
Aurelien BENARD <aurelien.benard@epitech.eu>
2021-11-25 01:29:38 +0100

    [Add] Postgres db (starting with docker-compose) and connection with sequelize [Add] Starting Google OAuth2

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,17 +1,58 @@
 // Constant
 const errorRoutes = require("./error");
-const sequelize = require("sequelize");
+const passport = require("passport");
 const router = require("express").Router();
 
+// Header
 router.use(function (_, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   next()
 })
+//
 
+// Home
 router.get('/', (_, res) => {
   res.status(200).send("Hello World");
 });
+//
 
+// Authentification
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+}
+
+router.get('/google', 
+  passport.authenticate(
+    'google', {scope: ["email", "profile"]}
+  )
+);
+
+router.get('/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/',
+  }),
+  function(req, res) {
+    res.redirect('/success');
+  }
+);
+
+router.get('/success', isLoggedIn, (req, res) => {
+  res.send("Welcome "+req.user.email);
+});
+
+router.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+});
+//
+
+
+// API
 router.get('/test', function(_, res) {
   res.status(200).send("test")
 });
@@ -21,6 +62,7 @@ router.get('/api/test', function(_, res) {
     msg: "Hello"
   })
 });
+//
 
 errorRoutes(router);
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,27 @@
 // Constant
-const PORT = 8080
-const HOST = "0.0.0.0";
+const {SERVER_HOST, SERVER_PORT} = process.env;
 const express = require("express");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 const serverRouter = require("./routes/routes.js");
+require("./passport.js");
+require("./models/index.js")
 
 // App
 const app = express();
+
+// Cookies
+app.use(cookieSession({
+  name: "google-auth-session",
+  keys: ["key1", "key2"]
+}));
+
+// Google OAuth2
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routing
 app.use('/', serverRouter);
 
-app.listen(PORT, HOST);
-console.log("Running on http://"+HOST+':'+PORT);
+app.listen(SERVER_PORT, SERVER_HOST);
+console.log("Running on http://"+SERVER_HOST+':'+SERVER_PORT);
